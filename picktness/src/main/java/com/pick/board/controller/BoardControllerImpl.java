04da4044c2136bed979aaf17f5fa8ababbe7d3aa
Administrator <Administrator@User -2023TJQCN>
2023-08-17 12:07:38 +0900

    board 자유게시판 오늘식단 수정

--- picktness/src/main/java/com/pick/board/controller/BoardControllerImpl.java
+++ picktness/src/main/java/com/pick/board/controller/BoardControllerImpl.java
@@ -16,4 +16,47 @@ public class BoardControllerImpl {
 		mav.setViewName(viewName);
 		return mav;
 	}
+	@RequestMapping(value="/board/millDetail.do")
+	public ModelAndView millDetail(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		ModelAndView mav = new ModelAndView();
+		String viewName = (String) request.getAttribute("viewName");
+		mav.setViewName(viewName);
+		return mav;
+}
+	@RequestMapping(value="/board/millList.do")
+	public ModelAndView millList(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		ModelAndView mav = new ModelAndView();
+		String viewName = (String) request.getAttribute("viewName");
+		mav.setViewName(viewName);
+		return mav;
+}
+	@RequestMapping(value="/board/noticeDetail.do")
+	public ModelAndView noticeDetail(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		ModelAndView mav = new ModelAndView();
+		String viewName = (String) request.getAttribute("viewName");
+		mav.setViewName(viewName);
+		return mav;
+}
+	@RequestMapping(value="/board/noticeList.do")
+	public ModelAndView noticeList(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		ModelAndView mav = new ModelAndView();
+		String viewName = (String) request.getAttribute("viewName");
+		mav.setViewName(viewName);
+		return mav;
+	}
+	
+	@RequestMapping(value="/board/freeBoardInsert.do")
+	public ModelAndView freeboardInsert(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		ModelAndView mav = new ModelAndView();
+		String viewName = (String) request.getAttribute("viewName");
+		mav.setViewName(viewName);
+		return mav;
+	}
+	@RequestMapping(value="/board/millBoardInsert.do")
+	public ModelAndView millBoardInsert(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		ModelAndView mav = new ModelAndView();
+		String viewName = (String) request.getAttribute("viewName");
+		mav.setViewName(viewName);
+		return mav;
+	}
 }
