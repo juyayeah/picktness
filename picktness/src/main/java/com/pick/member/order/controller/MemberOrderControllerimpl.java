d5357bf6d4d00bc7e16723913fc30f090e4c59b6
Administrator <Administrator@User -2023CAVLF>
2023-09-06 11:22:20 +0900

    장바구니에서 구매 페이지로 이동 후 구매 데이터 입력 완료 토탈 오더 테이블, 비지니스 테이블에 데이터 입력 포인트 히스토리 테이블 입력, 멤버 테이블 포인트 변동 입력

--- picktness/src/main/java/com/pick/member/order/controller/MemberOrderControllerimpl.java
+++ picktness/src/main/java/com/pick/member/order/controller/MemberOrderControllerimpl.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,10 +23,25 @@ import com.pick.goods.vo.GoodsBusinessVO;
 import com.pick.goods.vo.GoodsShoppingVO;
 import com.pick.goods.vo.GoodsTrainerVO;
 import com.pick.member.cart.vo.MemberCartVO;
+import com.pick.member.mypage.vo.MemberMypagePointVO;
+import com.pick.member.order.service.MemberOrderService;
+import com.pick.member.order.vo.MemberOrderBusinessVO;
+import com.pick.member.order.vo.MemberOrderShoppingVO;
 import com.pick.member.order.vo.MemberOrderTotalVO;
+import com.pick.member.service.MemberService;
 import com.pick.member.vo.MemberVO;
 @Controller("memberOrderController")
 public class MemberOrderControllerimpl implements MemberOrderController{
+	@Autowired
+	MemberOrderBusinessVO memberOrderBusinessVO;
+	@Autowired
+	MemberOrderShoppingVO memberOrderShoppingVO;
+	@Autowired
+	MemberOrderService memberOrderService;
+	@Autowired
+	MemberMypagePointVO memberPointVO;
+	@Autowired
+	MemberService memberService;
 	
 	
 	@Override
@@ -106,27 +122,84 @@ public class MemberOrderControllerimpl implements MemberOrderController{
 		String member_id = memberVO.getId();
 		Map orderMap = (Map) session.getAttribute("orderMap");
 		List<MemberCartVO> orderCartList = (List<MemberCartVO>) orderMap.get("orderCartList");
+		List<GoodsBusinessVO> orderBusinessList = (List<GoodsBusinessVO>) orderMap.get("orderBusinessList");
+		List<GoodsTrainerVO> orderTrainerList = (List<GoodsTrainerVO>) orderMap.get("orderTrainerList");
+		List<GoodsShoppingVO> orderShoppingList = (List<GoodsShoppingVO>) orderMap.get("orderShoppingList");
+		
+		List<MemberOrderBusinessVO> addOrderBusinessList = new ArrayList<MemberOrderBusinessVO>();
+		List<MemberOrderShoppingVO> addOrderShoppingList = new ArrayList<MemberOrderShoppingVO>();
+
 		MemberOrderTotalVO totalVO = new MemberOrderTotalVO();
 		LocalDate now = LocalDate.now();
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyMMdd");
 		String formatedNow = now.format(formatter);
 		int number = (int) (Math.random() * 8999) + 1000;
 		String order_num = formatedNow +  Integer.toString(number);
-		totalVO.setOrder_num(Integer.parseInt(order_num));
+		int use_point = Integer.parseInt(request.getParameter("use_point"));
+		int add_point = Integer.parseInt(request.getParameter("add_point"));
+		totalVO.setOrder_num(order_num);
 		totalVO.setMember_id(member_id);
 		totalVO.setTotal_price(request.getParameter("total_price"));
 		totalVO.setSale_price(request.getParameter("sale_price"));
 		totalVO.setPay_price(request.getParameter("pay_price"));
-		totalVO.setUse_point(Integer.parseInt(request.getParameter("use_point")));
-		totalVO.setAdd_point(Integer.parseInt(request.getParameter("add_point")));
+		totalVO.setUse_point(use_point);
+		totalVO.setAdd_point(add_point);
 		totalVO.setCredit_company(request.getParameter("credit_company"));
 		totalVO.setCredit_installment(request.getParameter("credit_installment"));
-		System.out.println(totalVO.getOrder_num());
-		System.out.println(totalVO.getCredit_company());
-		System.out.println(totalVO.getAdd_point());
 		
+		memberOrderService.addTotalOrder(totalVO);
 		
-		mav.setViewName("redirect:/main.do");
+		for(MemberCartVO memberCartVO : orderCartList) {
+			if(memberCartVO.getGoods_id().contains("pc")) {
+				for(GoodsBusinessVO businessVO : orderBusinessList) {
+					if(memberCartVO.getGoods_id().equals(businessVO.getGoods_id())) {
+						memberOrderBusinessVO.setMember_id(member_id);
+						memberOrderBusinessVO.setOrder_num(order_num);
+						memberOrderBusinessVO.setGoods_id(memberCartVO.getGoods_id());
+						memberOrderBusinessVO.setGoods_title(businessVO.getB_name());
+						String goods_option = memberCartVO.getGoods_option();
+						memberOrderBusinessVO.setGoods_option(goods_option);
+						switch(goods_option) {
+						case "prod1retail" : memberOrderBusinessVO.setGoods_price(businessVO.getProd1retail());
+							break;
+						case "prod3retail" : memberOrderBusinessVO.setGoods_price(businessVO.getProd3retail());
+							break;
+						case "prod6retail" : memberOrderBusinessVO.setGoods_price(businessVO.getProd6retail());
+							break;
+						case "prod12retail" : memberOrderBusinessVO.setGoods_price(businessVO.getProd12retail());
+							break;
+						}
+						addOrderBusinessList.add(memberOrderBusinessVO);
+					}
+				}
+			} else if(memberCartVO.getGoods_id().contains("tr")) {
+				
+			} else if(memberCartVO.getGoods_id().contains("sh")) {
+				
+			}
+		}
+		
+		if(addOrderBusinessList != null && addOrderBusinessList.size() != 0) {
+			memberOrderService.addBusinessOrder(addOrderBusinessList);
+		}
+		if(addOrderShoppingList != null && addOrderShoppingList.size() != 0) {
+			memberOrderService.addShoppingOrder(addOrderShoppingList);
+		}
+		memberPointVO.setMember_id(member_id);
+		if(use_point != 0) {
+			memberPointVO.setMember_point(use_point);
+			memberPointVO.setState("사용");
+			memberPointVO.setContent("구매 사용");
+			memberOrderService.useOrderPoint(memberPointVO);
+		}
+		memberPointVO.setMember_point(add_point);
+		memberPointVO.setState("적립");
+		memberPointVO.setContent("구매 적립");
+		memberOrderService.addOrderPoint(memberPointVO);
+		memberVO = memberService.selectById(member_id);
+		session.setAttribute("member", memberVO);
+		
+		mav.setViewName("redirect:/member/order/orderList.do");
 		return mav;
 	}
 
